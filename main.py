3946d7ff36e44976d5be3cb411fdd14d0c82cec9
Gabrie7ven <gabriel.oficial@yahoo.com>
2026-01-18 09:05:09 -0300

    .

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import math
 
 def isPrime(n):
     """Funcão para testar se um dado número inteiro é primo."""
-    
+    # Testando para valors ímpares
     if n < 2:
         return False
     if n != 2 and n % 2 == 0:
@@ -36,18 +36,17 @@ def loopStep(count,i):
 
 def main():
     speed(0)
-    count = 1
     limit = 200
-    contador = 1
+    counter = 1
     penup()
     for i in range(1,limit):
-        contador = loopStep(contador,i)
-        contador = loopStep(contador,i)
+        counter = loopStep(counter,i)
+        counter = loopStep(counter,i)
 
 
-
-main()
-mainloop()
+if __name__ == "__main__":
+    main()
+    mainloop()
 
 
 
